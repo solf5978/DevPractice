22be43b4cfc2b29f20d65f5f0a87160e39d3a2be
solf5978 <solf5978@vivaldi.net>
2022-01-05 08:15:40 +0800

    Add Chaptre 2 on ACM

---
+++ CnCPPPrac/ACMExampleInC/Ch02 - RudimentLooping/05-facSum.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int i, j, n, S = 0;
+    scanf("%d", &n);
+    for (i = 1; i <= n; i++)
+    {
+        int factorial = 1;
+        for (j = 1; j <= i; j++)
+            factorial = factorial * j;
+        S = S + factorial;
+    }
+    printf("%d\n", S % 1000000);
+    return 0;
+}
---
+++ CnCPPPrac/ACMExampleInC/Ch02 - RudimentLooping/07-simpleStat.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int x, n = 0, min, max, s = 0;
+    while (scanf("%d", &x) == 1)
+    {
+        s = s + x;
+        if (x < min)
+            min = x;
+        if (x > max)
+            max = x;
+        n++;
+    }
+    printf("%d %d %.3lf\n", min, max, (double)s/n);
+    return 0;
+}
